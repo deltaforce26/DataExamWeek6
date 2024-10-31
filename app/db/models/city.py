9289ea3248aa8fa1c1fafecf4041437b7d208edd
deltaforce26 <ahron052715@gmail.com>
2024-10-31 10:23:53 +0200

    country model

--- app/db/models/city.py
+++ app/db/models/city.py
@@ -1,12 +1,15 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, Float
+from sqlalchemy.orm import relationship
 
 from app.db.models import Base
 
 
 class City(Base):
-    __tablename__ = 'city'
+    __tablename__ = 'cities'
     city_id = Column(Integer, primary_key=True, autoincrement=True)
     city_name = Column(String)
-    country_id = Column(Integer, ForeignKey('country.id'))
     latitude = Column(Float)
     longitude = Column(Float)
+    country_id = Column(Integer, ForeignKey('country.country_id'))
+    country = relationship('Country', back_populates='cities')
+
